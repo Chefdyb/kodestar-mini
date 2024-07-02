b0f41d148b2cd49212e8f588c9d27a9e1011739d
Divine Quansah <divquan@gmail.com>
2024-07-02 19:18:16 +0000

    feat: add delete ui to files in sidebar

--- src/components/mycomponents/NavFiles.tsx
+++ src/components/mycomponents/NavFiles.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { MouseEvent } from "react";
+import { MouseEvent, useState } from "react";
 
 import { IFile } from "@/types";
 import FileIcon from "./FileIcon";
@@ -11,6 +11,24 @@ import {
   getIconForFolder,
   getIconForOpenFolder,
 } from "vscode-icons-js";
+import { Button } from "../ui/button";
+import {
+  ContextMenu,
+  ContextMenuContent,
+  ContextMenuItem,
+  ContextMenuTrigger,
+} from "@/components/ui/context-menu";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
 
 interface Props {
   files: IFile[];
@@ -19,6 +37,11 @@ interface Props {
 
 export default function NavFiles({ files, visible }: Props) {
   const { selected, setSelect, addToOpenedFiles, openedFiles } = useSource();
+
+  const [state, setState] = useState<"initial" | "renaming" | "deleting">(
+    "initial"
+  );
+  const [deleteDetails, setDeleteDetails] = useState<IFile | null>(null);
 
   const onShow = async (
     ev: React.MouseEvent<HTMLDivElement, MouseEvent>,
@@ -52,19 +75,65 @@ export default function NavFiles({ files, visible }: Props) {
         }
 
         return (
-          <div
-            //@ts-ignore
-            onClick={(ev) => onShow(ev, file)}
-            key={file.id}
-            className={`soure-item ${
-              isSelected ? "source-item-active " : ""
-            } flex items-center gap-2 px-2 py-0.5 text-gray-500 hover:text-gray-400 cursor-pointer `}
-          >
-            <img src={"/icons/" + getIconForFile(file.name)} className=" h-5" />
-            <span>{file.name}</span>
-          </div>
+          <AlertDialog>
+            <ContextMenu>
+              <ContextMenuTrigger>
+                <div
+                  //@ts-ignore
+                  key={file.id}
+                  className={`soure-item ${
+                    isSelected ? "source-item-active " : ""
+                  } flex items-center gap-2 px-2 py-0.5 text-gray-500 hover:text-gray-400 cursor-pointer `}
+                  onClick={(ev) => onShow(ev, file)}
+                >
+                  <img
+                    src={"/icons/" + getIconForFile(file.name)}
+                    className=" h-5"
+                  />
+                  <span>{file.name}</span>
+                </div>
+              </ContextMenuTrigger>
+              <ContextMenuContent>
+                <ContextMenuItem>Edit</ContextMenuItem>
+                <ContextMenuItem>Rename</ContextMenuItem>
+                <ContextMenuItem
+                  className="hover:bg-red-100 text-red-800"
+                  onClick={() => setDeleteDetails(file)}
+                >
+                  <AlertDialogTrigger>Delete</AlertDialogTrigger>
+                </ContextMenuItem>
+              </ContextMenuContent>
+              <DeleteModal fileToBeDeleted={deleteDetails} />
+            </ContextMenu>
+          </AlertDialog>
         );
       })}
     </div>
   );
 }
+
+const DeleteModal = ({
+  fileToBeDeleted,
+}: {
+  fileToBeDeleted: IFile | null;
+}) => {
+  if (!fileToBeDeleted) return null;
+  return (
+    <AlertDialogContent>
+      <AlertDialogHeader>
+        <AlertDialogTitle>
+          Are you absolutely sure you want to delete{" "}
+          <span className="text-red-800">{fileToBeDeleted.name}</span>?
+        </AlertDialogTitle>
+        <AlertDialogDescription>
+          This action cannot be undone. This will permanently delete your
+          account and remove your data from our servers.
+        </AlertDialogDescription>
+      </AlertDialogHeader>
+      <AlertDialogFooter>
+        <AlertDialogCancel>Cancel</AlertDialogCancel>
+        <AlertDialogAction>Continue</AlertDialogAction>
+      </AlertDialogFooter>
+    </AlertDialogContent>
+  );
+};
